7120ebfb3994147a05f5bae9f1448a4b328a992d
Nityam27 <73741814+Nityam27@users.noreply.github.com>
2021-04-26 17:00:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var bullet,wall;
+var thickness,weight,speed;
+
+
+function setup() {
+  
+  createCanvas(1200,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  car=createSprite(50, 200, 50,50);
+  car.velocityX = speed;
+  wall=createSprite(1100, 200, 60, height/2);
+  wall.shapeColor="blue";
+}
+
+function draw() {
+  background(0); 
+  
+  if(hasCollided(bullet, wall))
+  {
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed* speed/(thickness*thickness*thickness)
+    if(damage>10)
+    {
+      wall.shapeColor="red"
+    }
+
+    if(damage<10)
+    {
+      wall.shapeColor="green"
+    }
+  }
+ 
+  drawSprites();
+
+}
+
+function hasCollided(lbullet, lwall)
+{
+   bulletRightEdge=lbullet.x +lbullet.width;
+   walllLeftEdge=lwall.x;
+   if(bulletRightEdge>=wallLeftEdge)
+   { 
+     return true
+   }
+   return false;
+}
